44d89d03b5ffa2b8da8b069dc91a92bb0feed62a
Amnaazam123 <67511883+Amnaazam123@users.noreply.github.com>
2022-06-07 22:11:15 +0500

    final

---
+++ app/src/main/java/com/example/listviews_adapter/DetailActivity.java
@@ -0,0 +1,31 @@
+package com.example.listviews_adapter;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    ImageView img;
+    TextView name,id,position;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        img=findViewById(R.id.imageView);
+        name=findViewById(R.id.textView);
+        id=findViewById(R.id.textView2);
+        position=findViewById(R.id.textView3);
+
+        Intent intent=getIntent();
+        name.setText(intent.getStringExtra("name"));
+        id.setText(intent.getStringExtra("id"));
+        position.setText(intent.getStringExtra("position"));
+    }
+}
--- app/src/main/java/com/example/listviews_adapter/MainActivity.java
+++ app/src/main/java/com/example/listviews_adapter/MainActivity.java
@@ -2,40 +2,71 @@ package com.example.listviews_adapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn;
-    TextView task;
+    ArrayList<String> friendlist=new ArrayList<String>();
+    ListView listView;
+    ArrayAdapter<String> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn=findViewById(R.id.AddButton);
-        task=findViewById(R.id.task);
-        ListView list = findViewById(R.id.myListView);
+        friendlist.add("zain");
+        friendlist.add("Ahmed");
+        friendlist.add("Arooj");
 
-        ArrayList<String> todoList = new ArrayList<String>();
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,todoList);
+        listView = findViewById(R.id.lw1);
 
-        btn.setOnClickListener(new View.OnClickListener() {
+        adapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, friendlist);
+
+        Collections.sort(friendlist);
+
+        listView.setAdapter(adapter);
+        //delete record
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                if(task.getText()!=""){
-                    String todo = task.getText().toString();
-                    todoList.add(todo);
-                    list.setAdapter(arrayAdapter);
-                }
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d("adapter view ", adapterView.toString());
+                Log.d("view view ", view.toString());
+                Log.d("int i ", String.valueOf(i));
+                Log.d("long l ", String.valueOf(l));
+                Log.d("friend list", friendlist.get(i));
+                //delete
+                friendlist.remove(i);
+                adapter.notifyDataSetChanged();
             }
         });
+    }
+    //view details
+    //listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+    //  @Override
+    //public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+    //  Log.d("friend list",friendlist.get(i));
+    //Intent intent=new Intent(MainActivity.this,DetailActivity.class);
+    // intent.putExtra("name",friendlist.get(i));
+    //intent.putExtra("id","9");
+    //intent.putExtra("position","Software Engineer");
+    //startActivity(intent);
+    //}
+    //});
+    //};
+
+    public void Add(View view){
+        friendlist.add("Amna");
+        adapter.notifyDataSetChanged();
     }
 }
